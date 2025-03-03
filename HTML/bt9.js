1a80496f732365d83462b3fd283eb6a7e937b3b3
hongphuc112 <phucdinh044@gmail.com>
2025-03-04 06:51:56 +0700

    Add files via upload

---
+++ HTML/bt9.js
@@ -0,0 +1,16 @@
+let result1 = "javascript" + 5;
+console.log(`"javascript" + 5 =`, result1); // "javascript5"
+let result2 = "javascript" - 1;
+console.log(`"javascript" - 1 =`, result2); // NaN
+let result3 = "3" + 2;
+console.log(`"3" + 2 =`, result3); // "32"
+let result4 = "5" - 4;
+console.log(`"5" - 4 =`, result4); // 1
+let result5 = isNaN("123");
+console.log(`isNaN("123") =`, result5); // false
+let result6 = isNaN("hello");
+console.log(`isNaN("hello") =`, result6); // true
+let result7 = Number.isNaN("123");
+console.log(`Number.isNaN("123") =`, result7); // false
+let result8 = Number.isNaN(NaN);
+console.log(`Number.isNaN(NaN) =`, result8); // true
